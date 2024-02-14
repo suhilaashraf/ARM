deab96aef797dfbe33f0e07fe51e4bac00cd839c
suhila ashraf <suhilaa81@gmail.com>
2024-02-14 04:29:23 +0200

    upload RCC

---
+++ include/Drv/RCC.h
@@ -0,0 +1,159 @@
+#ifndef RCC_H
+#define RCC_H
+
+
+/*********BUSES PRESCALER****************/
+#define PrescalerAHB_2 0x00000080
+#define PrescalerAHB_4 0x00000090
+#define PrescalerAHB_8 0x000000A0
+#define PrescalerAHB_16 0x000000B0
+#define PrescalerAHB_64 0x000000C0
+#define PrescalerAHB_128 0x000000D0
+#define PrescalerAHB_256 0x000000E0
+#define PrescalerAHB_512 0x000000F0 // acts as a mask
+
+#define PrescalerAPB1_2 0x00001000
+#define PrescalerAPB1_4 0x00001400
+#define PrescalerAPB1_8 0x00001800
+#define PrescalerAPB1_16 0x00001C00 //acts as a mask
+
+#define PrescalerAPB2_2 0x00008000
+#define PrescalerAPB2_4 0x0000A000
+#define PrescalerAPB2_8 0x0000C000
+#define PrescalerAPB2_16 0x0000E000 //acts as a mask
+
+
+
+
+//***************PERIPHERALS****************//
+#define GPIOA BIT_0_MASK
+#define GPIOB BIT_1_MASK
+#define GPIOC BIT_2_MASK
+#define GPIOD BIT_3_MASK
+#define GPIOE BIT_4_MASK
+#define GPIOH BIT_7_MASK
+#define CRC BIT_12_MASK
+#define DMA1 BIT_21_MASK
+#define DMA2 BIT_22_MASK
+#define USB BIT_7_MASK
+#define TIM2 BIT_0_MASK
+#define TIM3 BIT_1_MASK
+#define TIM4 BIT_2_MASK
+#define TIM5 BIT_3_MASK
+#define WWDG BIT_11_MASK
+#define SPI2 BIT_14_MASK
+#define SPI3 BIT_15_MASK
+#define USART2 BIT_17_MASK
+#define I2C1 BIT_21_MASK
+#define I2C2 BIT_22_MASK
+#define I2C3 BIT_23_MASK
+#define PWR BIT_28_MASK
+#define TIM1 BIT_0_MASK
+#define USART1 BIT_4_MASK
+#define USART6 BIT_5_MASK
+#define ADC1 BIT_8_MASK
+#define SDIO BIT_11_MASK
+#define SPI1 BIT_12_MASK
+#define SPI4 BIT_13_MASK
+#define SYSCFG BIT_14_MASK
+#define TIM9 BIT_16_MASK
+#define TIM10 BIT_17_MASK
+#define TIM11 BIT_18_MASK
+
+
+
+typedef char uint8;
+typedef unsigned short uint16;
+typedef unsigned int uint32;
+
+/******SYSCLK OPTIONS *************/
+typedef enum
+{
+	Sysclk_HSI=0x00000001,
+	Sysclk_HSE=0x00010000,
+	Sysclk_PLL=0x01000000,
+}RCCSYSCLK_enu;
+
+typedef enum
+{
+	SysclkDisable,
+	SysclkEnable
+}RCCState_enu;
+
+/*******RCC ERROR CHECKS *************/
+typedef enum
+{
+	RCC_enuOK,
+	RCC_enuNOK,
+	RCC_enuInvalidPeripheral,
+	RCC_enuInvalidCLK,
+	RCC_enuCLKNotRDY,
+	RCC_enuCLKNotON,
+	RCC_enuDisabledDenied, //disable denied due to direct or indirect disable to the system clock
+	RCC_enuInvalidPRE, //invalid prescaler
+	RCC_enuInvaildParameter
+}RCCERRORCHECK_enu;
+/*******BUS options ************/
+typedef enum
+{
+	SysbusAHB1,
+	SysbusAHB2,
+	SysbusAPB1,
+	SysbusAPB2
+}RCCBUSES_enu;
+/*
+typedef enum
+{
+	CLKPRE_2=0x,
+	CLKPRE_4=4,
+	CLKPRE_8=8,
+	CLKPRE_16=16,
+	CLKPRE_64=64,
+	CLKPRE_128=128,
+	CLKPRE_256=256,
+	CLKPRE_512=512,
+}RCCBUSES_PRE_enu;*/
+
+/**********APIS*************/
+
+/* ConfigPLL funtion: set PLL parameters (M , N , P , Q )
+ * Caution:
+ * the max clk freq is 84Mhz make sure not to exceed it
+ * make sure not to exceed the parameters range
+ * the M  range: [ 2 :63]
+ * the N  range: [192 : 432]
+ * the Q  range: [2:15]
+ * the P  range: [ 2, 4, 6, or 8]
+ * */
+RCCERRORCHECK_enu ConfigPLL(uint16 M ,uint16 N,uint16 Q,uint16 P);
+
+
+/*
+ *RCCControlCLK FUNCTION: control CLK ON/OFF
+ *Sysclk :(Sysclk_HSI/ Sysclk_HSE/ Sysclk_PLL)
+ *caution: make sure when disable the required clk that it's not selected as system clock */
+RCCERRORCHECK_enu RCCControlCLK(RCCSYSCLK_enu Sysclk ,RCCState_enu SysclkSTATE );
+
+/*
+ *RCCSelectCLK FUNCTION: select CLK from:(Sysclk_HSI/ Sysclk_HSE/ Sysclk_PLL) to be the system clock
+ *caution: make sure to enable the clock and wait for it to get ready otherwise
+ the clk won't be selected as sysclk  */
+RCCERRORCHECK_enu RCCSelectCLK(RCCSYSCLK_enu Sysclk);
+
+
+
+/*RCC_SelectPRESCALER Function: select bus prescaler
+ * Prescaler options for APB1 ,APB2 :[2,4,8,16]
+ * Prescaler options for AHB1 :[2,4,8,16,64,128,256,512]*/
+RCCERRORCHECK_enu RCCSelectPRESCALER(RCCBUSES_enu Sysbus,uint16 Prescaler);
+
+/*RCCEnablePripheral Function: ENABLE
+ *
+*/
+
+RCCERRORCHECK_enu RCCEnablePripheral(uint32 Pripheral,RCCBUSES_enu Sysbus);
+
+
+
+
+#endif
---
+++ src/Drv/RCC.c
@@ -0,0 +1,363 @@
+#include "BITMASK.h"
+#include "RCC.h"
+
+
+/***********RCC REGISTERS****************/
+
+#define RCC_CR *((volatile uint32*)0x40023800)
+#define RCC_PLLCFGR *((volatile uint32*)0x40023804)
+#define RCC_CFGR *((volatile uint32*)0x40023808)
+#define RCC_CIR *((volatile uint32*)0x4002380C)
+#define RCC_AHB1ENR *((volatile uint32*)0x40023830)
+#define RCC_AHB2ENR *((volatile uint32*)0x40023834)
+#define RCC_APB1ENR *((volatile uint32*)0x40023840)
+#define RCC_APB2ENR *((volatile uint32*)0x40023844)
+#define RCC_AHB1LPENR *((volatile uint32*)0x40023850)
+#define RCC_AHB2LPENR *((volatile uint32*)0x40023854)
+#define RCC_APB1LPENR *((volatile uint32*)0x40023860)
+#define RCC_APB2LPENR *((volatile uint32*)0x40023864)
+
+
+/*********Registers bits************/
+#define SWS_MASK 0x0000000C
+#define SW_MASK 0x00000003
+#define PLLSRC_MASK 0x00400000 //For HSE Selected
+#define SW_HSI 0
+#define SW_HSE 1
+#define SW_PLL 2
+#define HSEBYP BIT_18_MASK
+
+#define SWS_HSI 0
+#define SWS_HSE 0x00000004
+#define SWS_PLL 0x00000008
+
+#define HSIRDY 0x00000001
+#define HSERDY 0x00020000
+#define PLLRDY 0x02000000
+
+#define PLLCFGR_MASK 0x0F037FC0
+
+/***********************IMPLEMENTATIONS*****************************/
+RCCERRORCHECK_enu RCCControlCLK(RCCSYSCLK_enu Sysclk,RCCState_enu SysclkSTATE)
+{
+	RCCERRORCHECK_enu ReturnErrorStatus_enu = RCC_enuNOK;
+	switch (Sysclk)
+	{
+	case Sysclk_HSI:
+		if (SysclkSTATE == SysclkEnable) //CLK ENABLE
+		{
+			if (RCC_CR &Sysclk_HSI)
+			{
+				ReturnErrorStatus_enu = RCC_enuOK;
+			}
+			else//should i wait until its ready?
+			{
+				RCC_CR |= Sysclk_HSI ;
+				ReturnErrorStatus_enu = RCC_enuOK;
+			}
+		}
+		else
+		{
+			if ( (RCC_CFGR&SWS_MASK)== SWS_HSI )
+			{
+				ReturnErrorStatus_enu= RCC_enuDisabledDenied;
+			}
+			else if ( ((RCC_CFGR&SWS_MASK)==(SWS_PLL)) && (!(RCC_PLLCFGR&PLLSRC_MASK)))
+			{
+				ReturnErrorStatus_enu= RCC_enuDisabledDenied;
+			}
+			else
+			{
+				RCC_CR &= ~ Sysclk_HSI ;
+				ReturnErrorStatus_enu = RCC_enuOK;
+			}
+		}
+		break;
+
+	case Sysclk_HSE :
+		if (SysclkSTATE == SysclkEnable) //CLK ENABLE
+		{
+			if (RCC_CR &Sysclk_HSE)
+			{
+				ReturnErrorStatus_enu = RCC_enuOK;
+			}
+			else
+			{
+				RCC_CR |=HSEBYP;
+				RCC_CR |=Sysclk_HSE ;
+				ReturnErrorStatus_enu = RCC_enuOK;
+			}
+
+		}
+		else
+		{
+			if ( (RCC_CFGR&SWS_MASK)==SWS_HSE)
+			{
+				ReturnErrorStatus_enu= RCC_enuDisabledDenied;
+			}
+			else if (((RCC_CFGR&SWS_MASK)==(SWS_PLL)) && (RCC_PLLCFGR&PLLSRC_MASK))
+			{
+				ReturnErrorStatus_enu= RCC_enuDisabledDenied;
+			}
+			else
+			{
+				RCC_CR &= ~Sysclk_HSE ;
+				ReturnErrorStatus_enu = RCC_enuOK;
+			}
+		}
+		break;
+
+	case Sysclk_PLL:
+		if (SysclkSTATE == SysclkEnable) //CLK ENABLE
+		{
+			if (RCC_CR &Sysclk_PLL)
+			{
+				ReturnErrorStatus_enu = RCC_enuOK;
+			}
+			else
+			{
+				RCC_CR |= Sysclk_PLL ;
+				ReturnErrorStatus_enu = RCC_enuOK;
+			}
+		}
+		else
+		{
+			if ( (RCC_CFGR&SWS_MASK) == (SWS_PLL) )
+			{
+				ReturnErrorStatus_enu= RCC_enuDisabledDenied;
+			}
+			else
+			{
+				RCC_CR &= ~Sysclk_HSE ;
+				ReturnErrorStatus_enu = RCC_enuOK;
+			}
+		}
+		break;
+
+	default:
+		ReturnErrorStatus_enu= RCC_enuInvalidCLK;
+		break;
+	}
+
+	return ReturnErrorStatus_enu;
+}
+
+
+
+RCCERRORCHECK_enu RCCSelectCLK(RCCSYSCLK_enu Sysclk)
+{
+	RCCERRORCHECK_enu ReturnErrorStatus_enu = RCC_enuNOK;
+	uint8 localmask;
+	switch(Sysclk)
+	{
+	case Sysclk_HSI:
+
+		if((RCC_CR&Sysclk_HSI)&&(RCC_CR&HSIRDY))
+		{
+			localmask = RCC_CFGR;
+			localmask &= ~SW_MASK ;
+			RCC_CFGR = localmask;
+			ReturnErrorStatus_enu = RCC_enuOK;
+		}
+		else
+		{
+			if(RCC_CR&Sysclk_HSI)
+			{
+				ReturnErrorStatus_enu = RCC_enuCLKNotRDY;
+
+			}
+			else
+			{
+				ReturnErrorStatus_enu = RCC_enuCLKNotON;
+			}
+		}
+		break;
+
+	case Sysclk_HSE:
+		if((RCC_CR&Sysclk_HSE)&&(RCC_CR&HSERDY))
+		{
+			localmask = RCC_CFGR;
+			localmask &= ~SW_MASK ;
+			localmask |= SW_HSE ;
+			RCC_CFGR = localmask;
+			ReturnErrorStatus_enu = RCC_enuOK;
+		}
+		else
+		{
+			if(RCC_CR&Sysclk_HSE)
+			{
+				ReturnErrorStatus_enu = RCC_enuCLKNotRDY;
+			}
+			else
+			{
+				ReturnErrorStatus_enu = RCC_enuCLKNotON;
+			}
+		}
+
+		break;
+
+	case Sysclk_PLL:
+		if((RCC_CR&Sysclk_PLL)&&(RCC_CR&PLLRDY))
+		{
+			localmask = RCC_CFGR;
+			localmask &= ~SW_MASK ;
+			RCC_CFGR |=SW_PLL;
+			RCC_CFGR = localmask;
+			ReturnErrorStatus_enu = RCC_enuOK;
+		}
+		else
+		{
+			if(RCC_CR&Sysclk_HSI)
+			{
+				ReturnErrorStatus_enu = RCC_enuCLKNotRDY;
+			}
+			else
+			{
+				ReturnErrorStatus_enu = RCC_enuCLKNotON;
+			}
+		}
+		break;
+
+	default :
+		ReturnErrorStatus_enu = RCC_enuInvalidCLK;
+		break;
+	}
+	return ReturnErrorStatus_enu;
+}
+
+RCCERRORCHECK_enu RCCSelectPRESCALER(RCCBUSES_enu Sysbus,uint16 Prescaler)
+{
+	RCCERRORCHECK_enu ReturnErrorStatus_enu = RCC_enuNOK;
+	uint8 localmask;
+	switch(Sysbus)
+	{
+	case SysbusAHB1:
+		if ((Prescaler<PrescalerAHB_2) &&(Prescaler>PrescalerAHB_512))
+		{
+			ReturnErrorStatus_enu = RCC_enuInvalidPRE;
+		}
+		else
+		{
+			localmask =RCC_CFGR;
+			localmask &= ~ PrescalerAHB_512;
+			localmask |= Prescaler;
+			RCC_CFGR=localmask;
+			ReturnErrorStatus_enu = RCC_enuOK;
+		}
+		break;
+
+	case SysbusAPB1:
+		if ((Prescaler<PrescalerAPB1_2) &&(Prescaler>PrescalerAPB1_16))
+		{
+			ReturnErrorStatus_enu = RCC_enuInvalidPRE;
+		}
+		else
+		{
+			localmask =RCC_CFGR;
+			localmask &= ~ PrescalerAPB1_16;
+			localmask |= Prescaler;
+			RCC_CFGR=localmask;
+			ReturnErrorStatus_enu = RCC_enuOK;
+		}
+
+		break;
+
+	case SysbusAPB2:
+		if ((Prescaler<PrescalerAPB2_2) &&(Prescaler>PrescalerAPB2_16))
+		{
+			ReturnErrorStatus_enu = RCC_enuInvalidPRE;
+		}
+		else
+		{
+			localmask =RCC_CFGR;
+			localmask &= ~ PrescalerAPB2_16;
+			localmask |= Prescaler;
+			RCC_CFGR=localmask;
+			ReturnErrorStatus_enu = RCC_enuOK;
+		}
+		break;
+	}
+	return ReturnErrorStatus_enu;
+}
+
+RCCERRORCHECK_enu ConfigPLL(uint16 M ,uint16 N,uint16 Q,uint16 P)
+{
+	RCCERRORCHECK_enu ReturnErrorStatus_enu = RCC_enuNOK;
+	uint16 localmask;
+	switch (P)
+	{
+	case 2:
+		P=0;
+		break;
+	case 4:
+		P=1;
+		break;
+	case 6:
+		P=2;
+		break;
+	case 8:
+		P=3;
+		break;
+	default:
+		ReturnErrorStatus_enu =RCC_enuInvaildParameter;
+		break;
+	}
+	if ((M<2)&&(M>63))
+	{
+		ReturnErrorStatus_enu =RCC_enuInvaildParameter;
+	}
+	else if ((N<192)&&(N>432))
+	{
+		ReturnErrorStatus_enu =RCC_enuInvaildParameter;
+	}
+	else if ((Q<2)&&(Q>15))
+	{
+		ReturnErrorStatus_enu =RCC_enuInvaildParameter;
+	}
+	else
+	{
+		ReturnErrorStatus_enu = RCC_enuOK;
+		localmask = RCC_PLLCFGR;
+		localmask &= ~ PLLCFGR_MASK;
+		localmask |= M | (N<< 6) | (P<<16) | (Q<<24);
+		RCC_PLLCFGR = localmask;
+	}
+	return ReturnErrorStatus_enu;
+}
+
+
+
+RCCERRORCHECK_enu RCCEnablePripheral(uint32 Pripheral,RCCBUSES_enu Sysbus)
+{
+	RCCERRORCHECK_enu ReturnErrorStatus_enu = RCC_enuNOK;
+
+	switch(Sysbus)
+	{
+	case SysbusAHB1:
+		RCC_AHB1ENR|=Pripheral;
+		ReturnErrorStatus_enu = RCC_enuOK;
+		break;
+
+	case SysbusAHB2:
+		RCC_AHB2ENR|=Pripheral;
+		ReturnErrorStatus_enu = RCC_enuOK;
+		break;
+	case SysbusAPB1:
+		RCC_APB1ENR|=Pripheral;
+		ReturnErrorStatus_enu = RCC_enuOK;
+		break;
+
+	case SysbusAPB2:
+		RCC_APB2ENR|=Pripheral;
+		ReturnErrorStatus_enu = RCC_enuOK;
+		break;
+
+	default:
+		ReturnErrorStatus_enu = RCC_enuInvalidPeripheral;
+		break;
+	}
+	return ReturnErrorStatus_enu;
+}
+
+
+
