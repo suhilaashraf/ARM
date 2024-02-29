a2cabf2e6220b2f3a24eec5184492f11defd65db
suhila ashraf <suhilaa81@gmail.com>
2024-02-29 19:36:47 +0200

    update switchtest

--- Testcases/Switch_test/test/test_Switch.c
+++ Testcases/Switch_test/test/test_Switch.c
@@ -20,23 +20,38 @@ void test_SWITCH_INIT(void)
 {
     GPIO_InitPin_IgnoreAndReturn(Status_enumOk);
     uint8_t result = SWITCH_INIT();
-   TEST_ASSERT_EQUAL_INT8 (Status_enumOk , result);
+    TEST_ASSERT_EQUAL_INT8 (Status_enumOk , result);
 }
 
+void test_SWITCH_INIT_1(void)
+{
+    GPIO_InitPin_IgnoreAndReturn(Status_enumParameterError);
+    uint8_t result = SWITCH_INIT();
+    TEST_ASSERT_EQUAL_INT8 (Status_enumParameterError, result);
+}
 
 void test_SWITCH_GETSTATUS(void)
 {
     GPIO_GetPinValue_IgnoreAndReturn(Status_enumOk);
     uint8_t result = SWITCH_GETSTATUS(Switch_one ,&status);
-   TEST_ASSERT_EQUAL_INT8 (Status_enumOk , result);
+    TEST_ASSERT_EQUAL_INT8 (Status_enumOk , result);
 }
 
 void test_SWITCH_GETSTATUS_1(void)
 {
     GPIO_GetPinValue_IgnoreAndReturn(Status_enumOk);
     uint8_t result = SWITCH_GETSTATUS(switch_three ,&status);
-   TEST_ASSERT_EQUAL_INT8 (Status_enumParameterError , result);
+    TEST_ASSERT_EQUAL_INT8 (Status_enumParameterError , result);
 }
+
+void test_SWITCH_GETSTATUS_2(void)
+{
+    GPIO_GetPinValue_IgnoreAndReturn(Status_enumParameterError);
+    uint8_t result = SWITCH_GETSTATUS(Switch_one ,&status);
+    TEST_ASSERT_EQUAL_INT8 (Status_enumParameterError , result);
+}
+
+
 
 
 #endif // TEST
